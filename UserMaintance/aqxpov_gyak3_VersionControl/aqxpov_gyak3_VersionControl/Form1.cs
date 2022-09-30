667eb43542c93ccd9e147c2b4fd0639ed4ad2da0
Virág Füredi <virag.furedi@stud.uni-corvinus.hu>
2022-09-30 12:26:04 +0200

    dev változások

--- UserMaintance/aqxpov_gyak3_VersionControl/aqxpov_gyak3_VersionControl/Form1.cs
+++ UserMaintance/aqxpov_gyak3_VersionControl/aqxpov_gyak3_VersionControl/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using aqxpov_gyak3_VersionControl.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,12 +13,18 @@ namespace aqxpov_gyak3_VersionControl
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
             label1.Text = Resource.LastName;
             label2.Text = Resource.FirstName;
             button1.Text = Resource.Add;
+
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
         }
 
         private void Form1_Load(object sender, EventArgs e)
