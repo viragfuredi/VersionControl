c984d8c3ffd60177764ed9ce6cc4109e717438c6
Virág Füredi <virag.furedi@stud.uni-corvinus.hu>
2022-10-07 11:53:37 +0200

    excel létrehozása //kivéve CreateTable()

--- UserMaintance/aqxpov_gyak04/aqxpov_gyak04/Form1.cs
+++ UserMaintance/aqxpov_gyak04/aqxpov_gyak04/Form1.cs
@@ -17,6 +17,11 @@ namespace aqxpov_gyak04
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
 
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +30,33 @@ namespace aqxpov_gyak04
         private void LoadData()
         {
             Flats = context.Flats.ToList();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWB.ActiveSheet;
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
