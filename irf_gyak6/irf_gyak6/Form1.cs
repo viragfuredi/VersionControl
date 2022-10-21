6c51d839bfbb149d44ad56dce7d219798ec908c0
Virág Füredi <virag.furedi@stud.uni-corvinus.hu>
2022-10-21 11:49:25 +0200

    4-es feladat feléig

--- irf_gyak6/irf_gyak6/Form1.cs
+++ irf_gyak6/irf_gyak6/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using irf_gyak6.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,27 @@ namespace irf_gyak6
 {
     public partial class Form1 : Form
     {
+        
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
