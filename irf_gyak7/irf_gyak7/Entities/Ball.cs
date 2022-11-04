2e7fdd8ccecb889c25ea66021ac6dd3da3be83f0
Virág Füredi <virag.furedi@stud.uni-corvinus.hu>
2022-11-04 12:14:52 +0100

    absztrakció a labdában

--- irf_gyak7/irf_gyak7/Entities/Ball.cs
+++ irf_gyak7/irf_gyak7/Entities/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using irf_gyak7.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -8,30 +9,12 @@ using System.Windows.Forms;
 
 namespace irf_gyak7.Entities
 {
-    public class Ball : Label
+    public class Ball : Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Width = 50;
-            Height = Width;
-            Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics g)
+       protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
-
-        public void MoveBall()
-            {
-            Left += 1;
-            }
-
+        
     }
 }
