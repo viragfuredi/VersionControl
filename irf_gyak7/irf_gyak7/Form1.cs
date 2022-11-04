428d1f583b0de93bc3b70f3213677419dc6ae874
Virág Füredi <virag.furedi@stud.uni-corvinus.hu>
2022-11-04 12:03:28 +0100

    futószalag

--- irf_gyak7/irf_gyak7/Form1.cs
+++ irf_gyak7/irf_gyak7/Form1.cs
@@ -38,7 +38,29 @@ namespace irf_gyak7
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
 
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > maxPosition)
+                    maxPosition = ball.Left;
+            }
+
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
         }
     }
 }
