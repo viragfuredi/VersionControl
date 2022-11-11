260a9e6253b0707895ab193621f83d873c7492c9
Virág Füredi <virag.furedi@stud.uni-corvinus.hu>
2022-11-11 11:53:24 +0100

    entities nélkül 4-es

--- irf_gyak8/irf_gyak8/Form1.cs
+++ irf_gyak8/irf_gyak8/Form1.cs
@@ -12,13 +12,19 @@ namespace irf_gyak8
 {
     public partial class Form1 : Form
     {
+        PortfolioDataSet context = new PortfolioDataSet();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'portfolioDataSet.Tick' table. You can move, or remove it, as needed.
+            this.tickTableAdapter.Fill(this.portfolioDataSet.Tick);
 
         }
     }
